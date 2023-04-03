79b8fee86a0412cc8224da8eca52d63f161fe7d9
Yuliya Platsitsyna <yuliya.platsitsyna@gmail.com>
2023-04-03 20:38:16 +0200

    add loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
+import NextNProgress from 'nextjs-progressbar';
 import "../styles/main.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <>
+      <NextNProgress />
       <Component {...pageProps} />
       {/* <style jsx global>{`
         body {
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,16 +1,25 @@
 import Router from "next/router";
 import { MainLayout } from "../../components/MainLayout";
 
-export default function About() {
+export default function About({ title }) {
   const btnClickHandler = () => {
     Router.push("/");
   };
 
   return (
     <MainLayout>
-      <h1>About Page</h1>
+      <h1>{title}</h1>
       <button onClick={btnClickHandler}>Go back to Home</button>
       <button onClick={() => Router.push("/posts")}>Go to Posts</button>
     </MainLayout>
   );
+}
+
+About.getInitialProps = async () => {
+  const response = await fetch('http://localhost:4200/about');
+  const data = await response.json();
+
+  return {
+    title: data.title
+  }
 }
